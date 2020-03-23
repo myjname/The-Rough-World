d374812e1a887dc607952cdb39b0704c8d9682a4
MyJname <s-2001@mail.ru>
2020-03-23 08:04:52 +0500

    новые блоки, сцена главного меню, задатки UI

---
+++ Assets/Scripts/Scene/SceneLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void SceneLoad(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+}
