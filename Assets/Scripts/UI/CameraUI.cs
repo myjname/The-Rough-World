b259ce7f9f8c6039db608f15d50be2042eb857ca
MyJname <s-2001@mail.ru>
2020-03-29 10:37:12 +0500

    доработка UI

---
+++ Assets/Scripts/UI/CameraUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraUI : MonoBehaviour
+{
+    public GameObject Panel;
+    private bool activated = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ActivateEscMenu();
+        }
+    }
+
+    //Открыть меню
+    public void ActivateEscMenu()
+    {
+        if (activated)
+        {
+            Panel.gameObject.SetActive(false);
+            activated = false;
+        }
+        else
+        {
+            Panel.gameObject.SetActive(true);
+            activated = true;
+        }
+
+    }
+
+    //Выход из игры
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    //Смена сцены
+    public void SceneLoad(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+
+
+}
--- Assets/Scripts/UI/MainMenuScript.cs
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
+public class MainMenuScript : MonoBehaviour
 {
+    //Выход из игры
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    
+    //Смена сцены
     public void SceneLoad(int SceneIndex)
     {
         SceneManager.LoadScene(SceneIndex);
